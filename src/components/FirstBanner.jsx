d5a7c847316f785b88b105926ebd72fd4c3fabec
ryan2625 <ryanfreas19@augustana.edu>
2023-11-30 19:18:05 -0600

    Editing first banner

--- src/components/FirstBanner.jsx
+++ src/components/FirstBanner.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import "../styles/first-banner.scss"
 
 function FirstBanner() {
   return (
-    <div>TRACK AND TRADE CRYTO</div>
+    <div className='first-banner'>
+      <h1>
+        The World’s Leading Cryptocurrency Platform
+      </h1>
+      <h2>
+        Buy Bitcoin, Ethereum, and all your favourite crypto
+      </h2>
+      <ul>
+        <li>
+          Trusted by more than 80M users world-wide
+        </li>
+        <li>
+        Leader in regulatory compliance and security certifications
+        </li>
+        <li>
+        The industry’s most comprehensive insurance coverage and verified proof of reserves
+        </li>
+      </ul>
+    </div>
   )
 }
 
