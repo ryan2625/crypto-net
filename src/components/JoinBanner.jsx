577af6f85bae51988fdfa63ac63415488e7702b2
ryan2625 <ryanfreas19@augustana.edu>
2023-12-04 01:57:41 -0600

    Need to find consistent way to calculate offset and add class to ref...

--- src/components/JoinBanner.jsx
+++ src/components/JoinBanner.jsx
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import image from "../assests/cryptoVisa1.jpg"
 import "../styles/join-banner.scss"
 
 function JoinBanner() {
+
+    const scrollRef = useRef(null)
+    const childScrollRef = useRef(null)
+
+    useEffect(() => {
+
+        const handleScroll = () => {
+            if (scrollRef.current && childScrollRef.current) {
+              const parentRect = scrollRef.current.getBoundingClientRect();
+              const yOffset = parentRect.top;
+              console.log(yOffset)
+
+              if (yOffset <= 500 && yOffset >= -400) {
+                setTimeout(() => {
+                     childScrollRef.current.classList.add('card-translation')
+                }, 200);
+              } else {
+                childScrollRef.current.classList.remove('card-translation');
+              }
+            }
+          };
+
+          handleScroll()
+    
+          window.addEventListener('scroll', handleScroll);
+          
+      }, []); 
+
   return (
-    <div className='join-banner'>
-        <div className="card">
+    <div className="total-banner">
+    <div className='join-banner' ref={scrollRef}>
+        <div className="card" ref={childScrollRef}>
             <img src={image} alt="" />
         </div>
-        
+    </div>
     </div>
   )
 }
