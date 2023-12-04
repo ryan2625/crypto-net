36750d597af9fa3e6c3ede5ff7702ae4cfb74269
ryan2625 <ryanfreas19@augustana.edu>
2023-12-04 00:19:27 -0600

    Editing sticky properties of div

--- src/components/JoinBanner.jsx
+++ src/components/JoinBanner.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import image from "../assests/cryptoVisa2.png"
+import image from "../assests/cryptoVisa1.jpg"
 import "../styles/join-banner.scss"
 
 function JoinBanner() {
   return (
     <div className='join-banner'>
-        <img src={image} alt="" />
+        <div className="card">
+            <img src={image} alt="" />
+        </div>
+        
     </div>
   )
 }
