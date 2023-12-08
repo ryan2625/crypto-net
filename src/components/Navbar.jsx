d509558e75c4be26fef1ed09fcb4be77bbbf846d
ryan2625 <ryanfreas19@augustana.edu>
2023-12-07 21:39:37 -0600

    Mobile nav tested

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../styles/navbar.scss"
 import image from "../assests/crypto-logo-official.png"
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Navbar() {
+
+  const [open, setOpen] = useState(false)
+
   return (
     <div className="navbar">
       <div className="nav-container">
@@ -10,17 +14,41 @@ function Navbar() {
         <ul>
           <li>
             <a href="#home">
-            Home
+              Home
             </a>
           </li>
           <li>
             <a href="#prices">
-            Prices
+              Prices
             </a>
           </li>
           <li>
             <a href="#rewards">
-            Rewards
+              Rewards
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="nav-container-mobile">
+        <img src={image} alt="" />
+        <li id="menu-icon" onClick={() => setOpen(!open)}>
+          <MenuIcon />
+        </li>
+        <ul className={open ? " shown mobile-ul" : "mobile-ul"}>
+          <li>
+            <a href="#home" onClick={() => setOpen(!open)}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#prices" onClick={() => setOpen(!open)}>
+              Prices
+            </a>
+          </li>
+          <li>
+            <a href="#rewards" onClick={() => setOpen(!open)}>
+              Rewards
             </a>
           </li>
         </ul>
